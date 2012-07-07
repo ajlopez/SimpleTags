091e54c98622272e1f47513d4f3c61aca7ec04e9
ajlopez <webmaster@ajlopez.com>
2012-07-07 07:06:09 -0300

    makeTag refactor

--- test/simple.js
+++ test/simple.js
@@ -5,6 +5,9 @@ var st = require('../'),
 assert.equal(st.h1(), "<h1 />");
 assert.equal(st.h1("Main"), "<h1>Main</h1>");
 
+assert.equal(st.h2(), "<h2 />");
+assert.equal(st.h2("Main"), "<h2>Main</h2>");
+
 
 
 
