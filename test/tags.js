6812cf026ba882ec7eb6c4307a65d7abc1cd0540
ajlopez <webmaster@ajlopez.com>
2013-01-15 05:32:43 -0300

    Get items using an object tag

--- test/tags.js
+++ test/tags.js
@@ -13,3 +13,10 @@ var item = engine.getItemById(itemId);
 assert.ok(item);
 assert.deepEqual(item, { id: itemId, data: 'contract.doc', tags: [{ author: 'adam' }, { date: '2013-01-14' }] });
 
+// Retrieve Items with Object Tags
+
+var items = engine.getItems([ { author: 'adam' } ]);
+assert.ok(items);
+assert.equal(items.length, 1);
+assert.deepEqual(items[0], { id: itemId, data: 'contract.doc', tags: [{ author: 'adam' }, { date: '2013-01-14' }] });
+
